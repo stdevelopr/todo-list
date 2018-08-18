b625caff218d3d53c2450a3c9c15b4dacd87afc3
stdevelopr <stdevelopr@gmail.com>
2018-08-17 21:53:21 -0300

    modal update

--- src/components/modal.js
+++ src/components/modal.js
@@ -75,33 +75,32 @@ class Modal extends Component {
 
 
   render(){
-   return(
-     <div>
-          <div className="modal fade" id="EditModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  }
-  <div className="modal-dialog modal-sm" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLabel">Edit</h5>
-        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+     // Bootstrap Modal
+    return(
+      <div>
+        <div className="modal fade" id="EditModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-sm" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Edit</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <input id="modal_input" type="text"/>
+                <label className="radio-inline"><input id="radio_pending" type="radio" name="optradio" checked/>Pending</label>
+                <label className="radio-inline"><input id="radio_completed" type="radio" name="optradio"/>Completed</label>
+              </div>
+              <div className="modal-footer">
+                <button id="modal_close" type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button id="modal_update" type="button" className="btn btn-primary" orig="all" onClick = {this.updateHandler}>Update</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="modal-body">
-        <input id="modal_input" type="text"/>
-        <label className="radio-inline"><input id="radio_pending" type="radio" name="optradio" checked/>Pending</label>
-    <label className="radio-inline"><input id="radio_completed" type="radio" name="optradio"/>Completed</label>
-      </div>
-      <div className="modal-footer">
-        <button id="modal_close" type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button id="modal_update" type="button" className="btn btn-primary" orig="all" onClick = {this.updateHandler}>Update</button>
-      </div>
-    </div>
-  </div>
-</div>
-     </div>
-   )
- }
+    )}
  };
 
  export default Modal;
